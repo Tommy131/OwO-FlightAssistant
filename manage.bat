3bee6fc1a02760e09813ecd7a42496e5c3dc3b44
Tommy131 <entwicklerKuhler3579@outlook.de>
2026-03-25 07:35:49 +0100

    chore: update manage.bat

--- manage.bat
+++ manage.bat
@@ -11,16 +11,18 @@ echo  [1] Clean ^& Get (flutter clean, pub get)
 echo  [2] Generate Icons (flutter_launcher_icons)
 echo  [3] Run Application (Windows) [DEFAULT - 3s]
 echo  [4] Build Release (Windows)
+echo  [5] Build Release (Android)
 echo.
 echo  [0] Exit
 echo.
 echo ======================================================
-echo Please select an option [1,2,3,4,0]:
-choice /C 12340 /N /T 3 /D 3 /M "Choice (Default is 3 in 3s): "
+echo Please select an option [1,2,3,4,5,0]:
+choice /C 123450 /N /T 3 /D 3 /M "Choice (Default is 3 in 3s): "
 
 :: Choice errorlevel is based on the index in /C
-if errorlevel 5 exit
-if errorlevel 4 goto BUILD
+if errorlevel 6 exit
+if errorlevel 5 goto BUILD_ANDROID
+if errorlevel 4 goto BUILD_WINDOWS
 if errorlevel 3 goto RUN
 if errorlevel 2 goto ICONS
 if errorlevel 1 goto CLEAN
@@ -52,10 +54,19 @@ echo === Running application on Windows ===
 call flutter run -d windows
 goto MENU
 
-:BUILD
+:BUILD_WINDOWS
 echo.
 echo === Building release for Windows ===
 call flutter build windows
+echo.
+echo Finished build process.
+pause
+goto MENU
+
+:BUILD_ANDROID
+echo.
+echo === Building release for Android ===
+call flutter build apk
 echo.
 echo Finished build process.
 pause
